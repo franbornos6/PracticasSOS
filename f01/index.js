91990817eddbe1c4b4187349c0c566f558d615c0
franbornos6 <fran_bornos6@hotmail.com>
2020-03-04 17:31:22 +0000

    f01 terminado

--- f01/index.js
+++ f01/index.js
@@ -1,2 +1,12 @@
-const cool = require("cool-ascii-faces");
-console.log(cool());
+const express = require("express");
+
+var app = express();
+var fecha= new Date();
+var hora = fecha.getHours()+":"+ fecha.getMinutes()+":"+ fecha.getSeconds();
+
+app.get("/time",(request, response) => {
+	response.send("<html>"+hora+"</html>");
+});
+app.listen(80)
+
+console.log("server ready");
