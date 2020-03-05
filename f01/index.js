f44b25f68af8bed53affb10d4a3865f5c338c99d
franbornos6 <fran_bornos6@hotmail.com>
2020-03-05 18:44:28 +0000

    f01 actualizado

--- f01/index.js
+++ f01/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
 var app = express();
-var fecha= new Date();
-var hora = fecha.getHours()+":"+ fecha.getMinutes()+":"+ fecha.getSeconds();
 
 app.get("/time",(request, response) => {
+	var fecha= new Date();
+	var hora = fecha.getHours()+":"+ fecha.getMinutes()+":"+ fecha.getSeconds();
 	response.send("<html>"+hora+"</html>");
 });
 app.listen(80)
