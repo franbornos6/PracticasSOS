d410cca2f9d96a699aa32767e8b7fcefc22a8642
franbornos6 <fran_bornos6@hotmail.com>
2020-03-22 19:09:22 +0000

    probando get

--- f03-API/index.js
+++ f03-API/index.js
@@ -1,1 +1,54 @@
-console.log("iioeiof");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 80;
+
+var motogp_statistics = [
+	{
+	country: "Spain",
+	pilot: { 
+			 name: "Jorge Lorenzo",
+             last_title: 2015,
+             world_title: 3,
+             victory: 47,
+             podium: 114
+             }
+	},
+	
+	{
+	country: "Spain",
+	pilot: { 
+			 name: "Marc Marquez",
+             last_title: 2019,
+             world_title: 6,
+             victory: 56,
+             podium: 95
+             }
+	}
+];
+
+const BASE_API_URL = "/api/v1/motogp_statistics";
+
+// GET 
+
+
+app.get(BASE_API_URL+"/country", (req,res) =>{
+	res.send(JSON.stringify(motogp_statistics,null,2));
+});
+
+// POST 
+
+app.post(BASE_API_URL+"/motogp_statistics",(req,res) =>{
+	motogp_statistics.push(req.body); 
+	res.sendStatus(201,"CREATED");
+});
+
+app.listen(port, () => {
+	console.log("Server ready");
+});
+
+console.log("Starting server...");
